79c9dacb1386ea6544db77b0fe5d24b3bbbca419
Larry <lawrenceh672@gmail.com>
2019-11-26 17:06:43 -0600

    tutorial 2

--- BirdBuddy/views.py
+++ BirdBuddy/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render   # Added for this step
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "BirdBuddy/index.html",  # Relative path from the 'templates' folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {
+            'title' : "Stream Processor",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
--- WildlifeWatcher/urls.py
+++ WildlifeWatcher/urls.py
@@ -5,14 +5,17 @@ Definition of urls for WildlifeWatcher.
 from datetime import datetime
 from django.urls import path
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.conf.urls import include, url
 from app import forms, views
+from django.contrib.auth.views import LoginView, LogoutView
 
 from django.conf.urls import include, url
 import BirdBuddy.views
 
 
 urlpatterns = [
+    url(r'^$', BirdBuddy.views.index, name='index'),
+    url(r'^home$', BirdBuddy.views.index, name='home'),
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
@@ -30,6 +33,4 @@ urlpatterns = [
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
-    url(r'^$', BirdBuddy.views.index, name='index'),
-    url(r'^home$', BirdBuddy.views.index, name='home'),
 ]
